a3bd8a9cf631747a6303f92f66058c0000f4ea6c
DiegoAVS <58373455+DiegoAVS@users.noreply.github.com>
2019-11-30 15:27:31 -0600

    100

---
+++ Diego.js
@@ -0,0 +1,136 @@
+//comentario
+/*comentario de ...
+varias lineas
+ */
+
+console.log ("Hello world");
+var greet = 'Hola';
+var nombre = 'Diego';
+console.log('greet')
+console.log (greet + ' ' + nombre);
+
+//operaciones
+var a = 1;
+var b = 2;
+var c = 3;
+
+var resultado = a + b + c;
+var resultado2 = a + b - c;
+console.log(resultado);
+
+
+var edad = 25;
+if(edad>=18){
+    console.log('Vamonos al Vanilla Unicorn');
+}else;
+    console.log("Vete a ver Pocoyo");
+
+
+    var a = 1
+    switch (1) {
+        case a = = 2;
+        console.log ('Option 1');
+       break;
+       case=a=1
+        console.log ('Option 2');
+        case=a=1
+    }
+
+
+    for (var a=12;a>=0;a--){
+        console.log(a);
+    }
+
+
+    function antro(){
+
+    }
+    antro=()=>{
+        if (edad>18){
+            return 'Vamonos de antro al Vanilla Unicorn';
+        }else{
+            return 'Vamos a ver Pocoyo';
+        
+        }
+    }
+
+    console.log(antro(17));
+    console.log(antro(19));
+    console.log(antro(15));
+    console.log(antro(20));
+    console.log(antro(16));
+
+    var x = 1;
+    var y = 2;
+    var resultado = x * y;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
